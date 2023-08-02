1d576886ab6c2ee884cee993b5c5920843467a12
Golam Morshed <45533293+GolamMorshed@users.noreply.github.com>
2023-08-02 13:44:17 +0100

    recording screen and lists

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:adult_story_book/screens/login.dart';
 import 'package:adult_story_book/screens/registration.dart';
 import 'package:adult_story_book/screens/dashboard.dart';
+import 'package:adult_story_book/screens/speech_to_text.dart';
+import 'package:adult_story_book/screens/recording_lists.dart';
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'dashboard',
+    initialRoute: 'recording_lists',
     routes: {
       'login': (context) => Login(),
       'registration': (context) => Registration(),
       'dashboard': (context) => Dashboard(),
+      'speech_to_text': (context) => SpeechToTextScreen(),
+      'recording_lists': (context) => AttractiveListViewScreen(),
     },
   ));
 }
