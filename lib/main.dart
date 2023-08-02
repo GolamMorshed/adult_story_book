31c26f40ab5ccc5c09af8ac291b3ebac864fc1c2
Golam Morshed <45533293+GolamMorshed@users.noreply.github.com>
2023-08-02 13:53:08 +0100

    storyboard design

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,18 @@ import 'package:adult_story_book/screens/registration.dart';
 import 'package:adult_story_book/screens/dashboard.dart';
 import 'package:adult_story_book/screens/speech_to_text.dart';
 import 'package:adult_story_book/screens/recording_lists.dart';
-
+import 'package:adult_story_book/screens/social_media.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'recording_lists',
+    initialRoute: 'social_media',
     routes: {
       'login': (context) => Login(),
       'registration': (context) => Registration(),
       'dashboard': (context) => Dashboard(),
       'speech_to_text': (context) => SpeechToTextScreen(),
       'recording_lists': (context) => AttractiveListViewScreen(),
+      'social_media': (context) => StoryBoard(),
     },
   ));
 }
