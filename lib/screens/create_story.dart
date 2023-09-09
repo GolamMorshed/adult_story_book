413a1f26f1e1c4664ead2a9d7daeafddc95cb30b
Golam Morshed <45533293+GolamMorshed@users.noreply.github.com>
2023-09-09 14:16:57 +0100

    update code

--- lib/screens/create_story.dart
+++ lib/screens/create_story.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:http/http.dart' as http;
@@ -27,6 +26,7 @@ class StoryInputPage extends StatefulWidget {
 }
 
 class _StoryInputPageState extends State<StoryInputPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController titleController = TextEditingController();
   String? selectedGenre;
   final stt.SpeechToText _speech = stt.SpeechToText();
@@ -69,32 +69,35 @@ class _StoryInputPageState extends State<StoryInputPage> {
   }
 
   Future<void> _saveStory() async {
-    String title = titleController.text;
-    String genre = selectedGenre ?? '';
-    String story = _text;
+    if (_formKey.currentState!.validate()) {
+      // Validation passed, proceed to save the story
+      String title = titleController.text;
+      String genre = selectedGenre ?? '';
+      String story = _text;
 
-    print(title);
-    print(genre);
-    print(story);
+      print(title);
+      print(genre);
+      print(story);
 
-    final apiUrl = 'http://127.0.0.1:8000/api/stories';
+      final apiUrl = 'http://127.0.0.1:8000/api/stories';
 
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'title': title,
-        'genre': genre,
-        'content': story,
-      }),
-    );
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'title': title,
+          'genre': genre,
+          'content': story,
+        }),
+      );
 
-    if (response.statusCode == 201) {
-      print('Story saved successfully');
-    } else {
-      print('Failed to save the story. Status Code: ${response.statusCode}');
+      if (response.statusCode == 201) {
+        print('Story saved successfully');
+      } else {
+        print('Failed to save the story. Status Code: ${response.statusCode}');
+      }
     }
   }
 
@@ -112,81 +115,102 @@ class _StoryInputPageState extends State<StoryInputPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Title:',
-              style: TextStyle(fontSize: 18),
-            ),
-            TextFormField(
-              controller: titleController,
-              decoration: InputDecoration(
-                hintText: 'Enter the story title',
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Title:',
+                style: TextStyle(fontSize: 18),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Genre:',
-              style: TextStyle(fontSize: 18),
-            ),
-            DropdownButtonFormField<String>(
-              value: selectedGenre,
-              items: <String>[
-                'Science Fiction',
-                'Fantasy',
-                'Romance',
-                'Mystery',
-                // Add more genre options as needed
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedGenre = newValue;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: 'Select the genre',
-                border: OutlineInputBorder(),
+              TextFormField(
+                controller: titleController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Title is required';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter the story title',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Story:',
-              style: TextStyle(fontSize: 18),
-            ),
-            // Replace TextFormField with a Row containing a TextFormField and a microphone button
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: TextEditingController(text: _text),
-                    maxLines: null, // Allows for multiple lines
-                    keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
-                      hintText: 'Speak or type your story here (up to 1000 words)',
-                      border: OutlineInputBorder(),
+              SizedBox(height: 16),
+              Text(
+                'Genre:',
+                style: TextStyle(fontSize: 18),
+              ),
+              DropdownButtonFormField<String>(
+                value: selectedGenre,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Genre is required';
+                  }
+                  return null;
+                },
+                items: <String>[
+                  'Science Fiction',
+                  'Fantasy',
+                  'Romance',
+                  'Mystery',
+                  // Add more genre options as needed
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedGenre = newValue;
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: 'Select the genre',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Story:',
+                style: TextStyle(fontSize: 18),
+              ),
+              // Replace TextFormField with a Row containing a TextFormField and a microphone button
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: TextEditingController(text: _text),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Story is required';
+                        }
+                        return null;
+                      },
+                      maxLines: null, // Allows for multiple lines
+                      keyboardType: TextInputType.multiline,
+                      decoration: InputDecoration(
+                        hintText: 'Speak or type your story here (up to 1000 words)',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.mic),
-                  onPressed: _listen,
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _saveStory,
-              child: Text('Save'),
-            ),
-            SizedBox(height: 16),
-          ],
+                  IconButton(
+                    icon: Icon(Icons.mic),
+                    onPressed: _listen,
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _saveStory,
+                child: Text('Save'),
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
